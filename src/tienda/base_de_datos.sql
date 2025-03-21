c832a0c746498c9c996fd06a457c8e12496830ed
Atom M. Nava <atommunoznava@gmail.com>
2025-03-20 19:59:03 -0600

    Update base_de_datos.sql

--- src/tienda/base_de_datos.sql
+++ src/tienda/base_de_datos.sql
@@ -1,2 +1,6 @@
 CREATE DATABASE IBEROMEX_P1;
 USE IBEROMEX_P1;
+
+INSERT INTO categoria_p1 (nombre) VALUES 
+('Electrónica'), ('Ropa'), ('Hogar'), ('Deportes'), ('Juguetes'),
+('Salud'), ('Belleza'), ('Alimentos'), ('Bebidas'), ('Automotriz');
